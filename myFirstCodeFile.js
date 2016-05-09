72af8e32036d729dc80a416d6dd1d6cff0c646ee
Justin Unbedacht <Justin@Justins-MacBook-Air.local>
2016-05-09 14:42:08 -0600

    commit

--- myFirstCodeFile.js
+++ myFirstCodeFile.js
@@ -3,5 +3,7 @@ var coding = "this is fun!"
 var journal = [];
 var again = "github is different"
 journal.push(dm11);
+journal.push(coding);
+journal.push(again);
 
-//to commit type in git add, git 
+//to commit type in git status, git add (file name),git commit -m "commit to"
